6e8f1e6f54eca1e563b8ed9d0198af09512b4009
shiran-trrzs <shirannnn.4@gmail.com>
2025-04-07 18:57:59 -0500

    config build

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,28 @@ import React, { useState, useEffect } from 'react';
 import ColumnChart from './components/ColumnChart';
 import RadialChart from './components/RadialChart';
 import AreaChart from './components/AreaChart';
+import './styles.css'
 
 const App = () => {
-    const [data, setData] = useState([]);
+    type GenerationMixItem = {
+        fuel: string;
+        perc: number;
+    };
+
+    type GenerationData = {
+        from: string;
+        to: string;
+        generationmix: GenerationMixItem[];
+    };
+
+    const [data, setData] = useState<GenerationData>({});
 
     useEffect(() => {
         const resolvedData = async () => {
             try {
 
                 const result = await axios.get("https://api.carbonintensity.org.uk/generation")
+                console.log('res', result.data.data);
                 setData([result.data.data]);
 
             } catch (error) {
@@ -34,21 +47,32 @@ const App = () => {
     const perc = data[0].generationmix.map((item) => item.perc);
     const fuel = data[0].generationmix.map((item) => item.fuel.toUpperCase());
 
+    if (!perc.length) {
+        return <div>Loading...</div>;
+    }
+
+    console.log('perc', perc);
     return (
-        <div>
+        <div className="totalContainer">
             <h1> Last update {lastDate} {lastHour} </h1>
             <section>
+            <div className="chartContainer">
                 <ColumnChart
                     perc={perc}
                     fuel={fuel}
                 />
+            </div>
+            <div className="chartContainer">
                 <RadialChart
                     perc={perc}
                     fuel={fuel}
                 />
+            </div>
+            <div className="chartContainer">
                 <AreaChart
                     data={data[0].generationmix}
                 />
+            </div>
             </section>
         </div>
     )
